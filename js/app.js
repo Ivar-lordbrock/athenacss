bdc8ca6eafcdbe0bfda69ad21894b4deba584d28
julien <juliengauducheau17@gmail.com>
2019-03-01 15:48:16 +0100

    add navbar & resolve bug

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ btnClose.addEventListener('click', function(){
     const alert = document.getElementById('alert');
     alert.parentNode.removeChild(alert)
 });
+
+
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
